36e42abff7c7c46f98087c9db8f4883c80a40cb3
wangefan <wangefan@gmail.com>
2018-03-01 11:11:53 +0800

    [What] 1.Refine code 2.查loss的變化

--- tools/train.py
+++ tools/train.py
@@ -1,6 +1,5 @@
 import sys
 import os.path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from optparse import OptionParser
 import os
 from utils.text_dataset import TextDataSet
@@ -9,15 +8,17 @@ import tools.models as mo
 import tensorflow as tf
 import time
 
+# 讓import module可以找到，解決import error
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append('./')
+
 parser = OptionParser()
-parser.add_option("-c", "--conf", dest="configure",  
-                  help="configure filename")
+parser.add_option("-c", "--conf", dest="configure", help="configure filename")
 (options, args) = parser.parse_args() 
 if options.configure:
   conf_file = str(options.configure)
 else:
-  print('please sspecify --conf configure filename')
+  print('please specify --conf configure filename')
   exit(0)
 
 common_params, data_set_params, models_params = process_config(conf_file)
@@ -46,7 +47,7 @@ with tf.Session(config=config) as sess:
     # 1. initialize variables
     sess.run(tf.global_variables_initializer())
 
-
+    # 2. 建立saver物件，todo:從未train完的model restore後繼續train
     saver = tf.train.Saver()
     # saver.restore(session, os.path.join(MODEL_PATH, "model"))
 
@@ -55,7 +56,7 @@ with tf.Session(config=config) as sess:
     last_epoch = -1
     while dataSet.epoch_completed() < dataSet.epoch:
         (imgs, rect_labels) = dataSet.batch()
-        train_step.run(feed_dict={model.img_in_placeholder: imgs, model.rect_label_placeholder: rect_labels})
+        loss_val, _ = sess.run([loss, train_step], feed_dict={model.img_in_placeholder: imgs, model.rect_label_placeholder: rect_labels})
         if dataSet.epoch_completed() > last_epoch:
             last_epoch = dataSet.epoch_completed()
             # score_test = loss.eval(feed_dict={model.img_in_placeholder: X2_test, model.rect_label_placeholder: Y2_test})
